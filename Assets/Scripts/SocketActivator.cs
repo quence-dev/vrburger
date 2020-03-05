9b2c47930225839d6876d32a07a7ac0b5fbabeee
quence-dev <60503673+quence-dev@users.noreply.github.com>
2020-03-04 17:13:56 -0800

    SocketDecouple
    Decoupled socket from grabItem script by adding activator objects to each prefab, which activate onSelectEnter. Not sure if I like it yet. Also renamed the scene

---
+++ Assets/Scripts/SocketActivator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SocketActivator : MonoBehaviour
+{
+    public bool Toggle = false;
+    public UnityEvent OnActivated;
+    public UnityEvent OnDeactivated;
+
+    bool m_Activated = false;
+
+    public void Activated()
+    {
+        if (Toggle)
+        {
+            if (m_Activated)
+                OnDeactivated.Invoke();
+            else
+                OnActivated.Invoke();
+            m_Activated = !m_Activated;
+        }
+        else
+        {
+            Debug.Log("Socket activated");
+            OnActivated.Invoke();
+            m_Activated = true;
+        }
+    }
+
+    public void Deactivated()
+    {
+        if (!Toggle)
+        {
+            OnDeactivated.Invoke();
+            m_Activated = false;
+        }
+    }
+}
